846e6616f464b3c4c414b9c76954ffea2f4a5c14
Karoline Dias Barreto <karolinedbarreto@gmail.com>
2021-09-16 17:18:05 -0300

    refactor: alteração do nome do arquivo para ResponseAPI

---
+++ app/Traits/ResponseAPI.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Traits;
+use App\Enum\HttpStatusCode;
+
+trait ResponseAPI
+{
+    public function coreResponse(int $statusCode, bool $isSuccess, $data, $errorValidator, string $message = '')
+    {
+        if($isSuccess) {
+            return response()->json([
+                'results' => $data
+            ], $statusCode);
+        } else {
+            return response()->json([
+                'message' => $message,
+                'errors' => $errorValidator,
+            ], $statusCode);
+        }
+    }
+
+    public function success(int $statusCode, $data = [])
+    {
+        return $this->coreResponse(
+            $statusCode, 
+            true, 
+            $data, 
+            []
+        );
+    }
+
+    public function error(int $statusCode, string $message = '', $errorValidator = [])
+    {
+        if($statusCode == 0)
+            $statusCode = HttpStatusCode::INTERNAL_ERROR;
+
+        return $this->coreResponse(
+            $statusCode, 
+            false, 
+            [], 
+            $errorValidator,
+            $message
+        );
+    }
+}
